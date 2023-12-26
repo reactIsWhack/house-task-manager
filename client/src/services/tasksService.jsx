6b3dc67438129304931f53ee8854521201833b47
Ryan Naini <ryan.naini29@gmail.com>
2023-12-26 04:52:55 -0500

    Fixed server url bugs

--- client/src/services/tasksService.jsx
+++ client/src/services/tasksService.jsx
@@ -2,42 +2,57 @@ import axios from 'axios';
 
 export const createTask = async (formData) => {
   const { name, monthly, week } = formData;
-  const response = await axios.post('/api/tasks', {
-    name,
-    type: monthly ? 'monthly' : 'weekly',
-    week: Number(week),
-  });
+  const response = await axios.post(
+    'https://house-task-manager-server.onrender.com/api/tasks',
+    {
+      name,
+      type: monthly ? 'monthly' : 'weekly',
+      week: Number(week),
+    }
+  );
   return response.data;
 };
 
 export const getAllTasks = async () => {
-  const { data } = await axios.get('/api/tasks');
+  const { data } = await axios.get(
+    'https://house-task-manager-server.onrender.com/api/tasks'
+  );
   return data;
 };
 
 export const completePatch = async (id) => {
-  const { data } = await axios.patch(`/api/tasks/completetask/${id}`);
+  const { data } = await axios.patch(
+    `https://house-task-manager-server.onrender.com/api/tasks/completetask/${id}`
+  );
   return data;
 };
 
 export const taskDeleted = async (id) => {
-  const response = await axios.delete(`/api/tasks/deletetask/${id}`);
+  const response = await axios.delete(
+    `https://house-task-manager-server.onrender.com/api/tasks/deletetask/${id}`
+  );
   return response;
 };
 
 export const editTask = async (formData) => {
   const { id, name, week } = formData;
-  const { data } = await axios.patch(`/api/tasks/updatetask/${id}`, {
-    name,
-    week: Number(week),
-  });
+  const { data } = await axios.patch(
+    `https://house-task-manager-server.onrender.com/api/tasks/updatetask/${id}`,
+    {
+      name,
+      week: Number(week),
+    }
+  );
   return data;
 };
 
 export const moveTask = async (data) => {
   const { id, week } = data;
-  const response = await axios.patch(`/api/tasks/movetask/${id}`, {
-    week: Number(week),
-  });
+  const response = await axios.patch(
+    `https://house-task-manager-server.onrender.com/api/tasks/movetask/${id}`,
+    {
+      week: Number(week),
+    }
+  );
   return response.data;
 };
